7921f5fdf42c9e11449bac890f4c678900e7a910
zipp080 <EricThibeau@gmail.com>
2016-03-06 00:17:59 -0800

    Commit

--- JobSeekersFinal/Controllers/HomeController.cs
+++ JobSeekersFinal/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace JobSeekersFinal.Controllers
             return View();
         }
 
+        public ActionResult Main()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
