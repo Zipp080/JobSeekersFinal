f99805bee825f18e26f2d2dac7334cd83545b532
Eric Thibeau <EricThibeau@gmail.com>
2016-03-05 16:10:41 -0800

    First Commit

---
+++ JobSeekersFinal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JobSeekersFinal.Startup))]
+namespace JobSeekersFinal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ dbo.Employee.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ANSWERSCORE]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [AnswerID] INT NULL, 
+    [CategoryID] INT NULL, 
+    [Score] INT NULL
+)
